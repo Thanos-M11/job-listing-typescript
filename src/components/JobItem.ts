51b63e9b8c7246ebcfa327d9ee2863f0c8f1b7cf
Thanos-M11 <kalaitzisa@gmail.com>
2024-08-31 14:02:15 +0300

    update app.css

--- src/components/JobItem.ts
+++ src/components/JobItem.ts
@@ -28,6 +28,10 @@ export class JobItem extends Component<HTMLUListElement, HTMLLIElement> {
     this.job.new && this.removeInactiveClass("new");
     this.job.featured && this.removeInactiveClass("featured");
 
+    if (this.job.new && this.job.featured) {
+      this.cloneElement.classList.add("border-left");
+    }
+
     const jobFilterArticle = this.cloneElement.querySelector(".job-filters");
     jobFilterArticle!.id = `jf-${this.job.id}`;
 
